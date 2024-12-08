eb8a178fc1374ef4b5ab74229471defe007755f6
HyunFlow <yonghyunlee.dev@gmail.com>
2024-12-08 23:07:38 +0100

    AnalyticCounter Refactoring Updated.

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -7,13 +7,9 @@ import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
 	
-	/* The method creates a file named 'result.out' from the data provided in symptoms. */
+/* The method creates a file named 'result.out' from the data provided in symptoms. */	
 	@Override
 	public void writeSymptoms(Map<String, Integer>symptoms) {
-		
-		/* symptoms data check. Map<String, Integer> symptomsData come here. */
-		System.out.println(symptoms); 
-		
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"));
 			for(Map.Entry<String, Integer> entry : symptoms.entrySet()) {
